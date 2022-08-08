833b49f381986432e555ae8c13a9f3b790bea910
mattand <mjande@umich.edu>
2022-08-08 11:32:26 -0400

    Create db and airport table with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport::delete_all
+
+airport_codes = %w(ATL DFW DEN ORD LAX CLT MCO LAS PHX MIA SEA IAH JFK EWR FLL MSP SFO DTW BOS SLC PHL BWI TPA SAN LGA MDW BNA IAD)
+
+airport_codes.each do |airport_code|
+  Airport.create(code: airport_code)
+end
