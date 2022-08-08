c71e3d3431b9b6db592b45638f534c8c19fce4e3
mattand <mjande@umich.edu>
2022-08-08 13:18:31 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Airport::delete_all
+Flight.delete_all
+Airport.delete_all
+
 
 airport_codes = %w(ATL DFW DEN ORD LAX CLT MCO LAS PHX MIA SEA IAH JFK EWR FLL MSP SFO DTW BOS SLC PHL BWI TPA SAN LGA MDW BNA IAD)
 
 airport_codes.each do |airport_code|
   Airport.create(code: airport_code)
+end
+
+15.times do
+  orig = Airport.all.sample
+  dest = Airport.where.not(id: orig.id).sample
+  dur = rand(3..10)
+  start = rand(Time.now..Time.now.next_year)
+  new_flight = Flight.create(origin_id: orig.id, destination_id: dest.id, departure_time: start, flight_duration: dur)
 end
