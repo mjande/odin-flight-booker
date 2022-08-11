edc392fefab89a59a568f48b044123c45a5fc5a6
mattand <mjande@umich.edu>
2022-08-11 10:21:30 -0400

    Create create#booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,21 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new(flight_id: params[:flight_id])
     num_of_passengers = params[:num_of_passengers].to_i
-    num_of_passengers.times { @booking.passengers.build }
+    num_of_passengers.times { @booking.passengers.build(flight_id: params[:flight_id]) }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      render :booking
+    else
+      redirect_to :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
