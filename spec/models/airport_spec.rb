29f11d40006a767c8c67ee2266e48798308d2ac8
mattand <mjande@umich.edu>
2022-08-12 11:35:06 -0400

    Create Airport model specs

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
-  it 'validates presence of airport code'
+  it 'is valid with valid attributes' do
+    expect(Airport.new(code: 'BOS')).to be_valid
+  end
+  it 'is not valid with missing airport code' do
+    expect(Airport.new).not_to be_valid
+  end
 end
