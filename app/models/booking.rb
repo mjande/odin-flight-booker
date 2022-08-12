7b03a4d60365a72b77879e459ff34ec92b0efc4e
mattand <mjande@umich.edu>
2022-08-12 12:50:21 -0400

    Create specs for Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@
 
 class Booking < ApplicationRecord
   belongs_to :flight
+
   has_many :passengers, dependent: :delete_all
   accepts_nested_attributes_for :passengers
+  validates :passengers, presence: true
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:origin) { Airport.new(code: 'BOS') }
+  let!(:destination) { Airport.new(code: 'SEA') }
+  let!(:flight) { Flight.create(origin:, destination:, departure_time: Time.zone.now, flight_duration: 1) }
+  let(:passenger_params) { { name: 'Joe', email: 'joe@gmail.com' } }
+
+  it 'is valid with valid attributes' do
+    booking = Booking.new(flight_id: flight.id)
+    booking.passengers.build(passenger_params)
+    expect(booking).to be_valid
+  end
+
+  it 'is not valid with missing flight' do
+    booking = Booking.new
+    booking.passengers.build(passenger_params)
+    expect(booking).to be_invalid
+  end
+
+  it 'is not valid with no passengers' do
+    expect(Booking.new(flight_id: flight.id)).to be_invalid
+  end
 end
