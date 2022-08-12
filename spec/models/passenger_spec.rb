e72bee846a067042c7db1b7afd5b99a9dd68bd8e
mattand <mjande@umich.edu>
2022-08-12 17:58:49 -0400

    Create passenger model specs

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(build(:passenger)).to be_valid
+  end
+
+  it 'is not valid with missing name' do
+    expect(build(:passenger, name: nil)).to be_invalid
+  end
+
+  it 'is not valid with missing email' do
+    expect(build(:passenger, email: nil)).to be_invalid
+  end
 end
