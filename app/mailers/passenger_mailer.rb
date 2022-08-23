e7fa62f8eb7a1ee90bc8c1e269739fbb2d44cea1
mattand <mjande@umich.edu>
2022-08-23 12:07:46 -0400

    Build passenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notification@flightbooker.com'
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @booking_url = url(params[:booking])
+    mail(to: @passenger.email, subject: 'Booking Confirmation from Flight Booker')
+  end
 end
