cf6e4dfa199abf8029fd1442528640f1e1551400
mattand <mjande@umich.edu>
2022-09-01 09:58:23 -0400

    Fix typo for smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    username: ENV['gmail_username'],
+    user_name: ENV['gmail_username'],
     password: ENV['gmail_password'],
     authentication: 'plain',
     enable_starttls_auto: true
